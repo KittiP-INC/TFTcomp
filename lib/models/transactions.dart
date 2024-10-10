9831fa47c0df5288c7ac9a14c0683e6165a68520
KittiP-INC <zazaul14@gmail.com>
2024-10-10 16:54:48 +0700

    first commit

---
+++ lib/models/transactions.dart
@@ -0,0 +1,15 @@
+class Transactions {
+  int? keyID;
+  final String compname;
+  final String character;
+  final String augment;
+  final String emblem;
+
+  Transactions({
+    this.keyID,
+    required this.compname,
+    required this.character,
+    required this.augment,
+    required this.emblem,
+  });
+}
---
+++ lib/screens/edit_screen.dart
@@ -0,0 +1,116 @@
+import 'package:account/main.dart';
+import 'package:account/models/transactions.dart';
+import 'package:account/provider/transaction_provider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EditScreen extends StatefulWidget {
+  Transactions statement;
+
+  EditScreen({super.key, required this.statement});
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  final formKey = GlobalKey<FormState>();
+  final compnameController = TextEditingController();
+  final characterController = TextEditingController();
+  final augmentController = TextEditingController();
+  final emblemController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    compnameController.text = widget.statement.compname;
+    characterController.text = widget.statement.character;
+    augmentController.text = widget.statement.compname;
+    emblemController.text = widget.statement.compname;
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('แบบฟอร์มแก้ไขข้อมูล'),
+        ),
+        body: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'ชื่อComp',
+                  ),
+                  autofocus: false,
+                  controller: compnameController,
+                  validator: (String? str) {
+                    if (str!.isEmpty) {
+                      return 'กรุณากรอกข้อมูล';
+                    }
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'ชื่อตัวละคร',
+                  ),
+                  autofocus: false,
+                  controller: characterController,
+                  validator: (String? str) {
+                    if (str!.isEmpty) {
+                      return 'กรุณากรอกข้อมูล';
+                    }
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'ชื่อAugment',
+                  ),
+                  autofocus: false,
+                  controller: augmentController,
+                  validator: (String? str) {
+                    if (str!.isEmpty) {
+                      return 'กรุณากรอกข้อมูล';
+                    }
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'ชื่อEmblem',
+                  ),
+                  autofocus: false,
+                  controller: emblemController,
+                  validator: (String? str) {
+                    if (str!.isEmpty) {
+                      return 'กรุณากรอกข้อมูล';
+                    }
+                  },
+                ),
+                TextButton(
+                    child: const Text('บันทึก'),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        // create transaction data object
+                        var statement = Transactions(
+                          keyID: null,
+                          compname: compnameController.text,
+                          character: characterController.text,
+                          augment: augmentController.text,
+                          emblem: emblemController.text,
+                        );
+                        // add transaction data object to provider
+                        var provider = Provider.of<TransactionProvider>(context,
+                            listen: false);
+
+                        provider.updateTransaction(statement);
+
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                fullscreenDialog: true,
+                                builder: (context) {
+                                  return MyHomePage();
+                                }));
+                      }
+                    })
+              ],
+            )));
+  }
+}
